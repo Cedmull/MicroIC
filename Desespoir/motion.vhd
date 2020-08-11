9f3cba757e3c1f84385fca27b4ea154d10501b03
Cedmull <58887195+Cedmull@users.noreply.github.com>
2020-08-11 22:13:20 +0200

    start les deux joueurs

--- Desespoir/motion.vhd
+++ Desespoir/motion.vhd
@@ -23,14 +23,14 @@ port
 	push_start : in std_logic; -- if start lololol
 	
 	-- Control and initialization variables
-	player_flag   : in std_logic; -- '0' for player 1, '1' if player 2
+	player_id   : in std_logic; -- '0' for player 1, '1' if player 2
 	start_game	  : in std_logic; -- '0' to start
 	-- OUTPUTs:
 	-- Player position and orientation
 	x_player     : buffer natural range 0     to 600;
-	y_player     : buffer natural range 0     to 500;
+	y_player     : buffer natural range 0     to 600;
 	x_player_2 	 : in natural 		range 0 		to 600;
-	y_player_2	 : in natural 		range 0 		to 500;
+	y_player_2	 : in natural 		range 0 		to 600;
 	cnt_move  	 : buffer natural range 0 		to 100000
 );
 end motion;
@@ -39,12 +39,12 @@ architecture motion_arch of motion is
 	
 begin
 	
-	get_player_position: process
+	move: process
 		                  -- These variables are used in order to avoid moving the player while being 
-			variable memo_x : integer range 0 	to 600;		-- displayed and also prevents the player to "enter" in a wall
-			variable memo_y : integer range 0 	to 600;
-			variable memo_x_2 : integer range 0 to 600;
-			variable memo_y_2 : integer range 0 to 600;
+			variable tmp_x : integer range 0 	to 600;		-- displayed and also prevents the player to "enter" in a wall
+			variable tmp_y : integer range 0 	to 600;
+			variable tmp_x_2 : integer range 0 to 600;
+			variable tmp_y_2 : integer range 0 to 600;
 			variable counter :integer range 0 	to 100000;
 			variable pos_diff_x : integer range -1000  to 1000;
 			variable pos_diff_y : integer range -1000  to 1000;
@@ -55,30 +55,30 @@ begin
 		
 		wait until rising_edge( CLOCK_50 );
 		
-		if(push_start = '1') then
-			if player_flag = '0' then
-				memo_x := 200;
-				memo_x_2:= 600;
+		if(start_game = '0') then
+			if player_id = '0' then
+				tmp_x := 200;
+				x_player <= tmp_x;
 			else
-				memo_x := 600;
-				memo_x_2 := 200;
+				tmp_x := 600;
+				x_player <= tmp_x;
 			end if;
-			memo_y := 300;	
+			tmp_y := 300;	
 			
-			x_player <= memo_x;
-			y_player <= memo_y;
+			
+			y_player <= tmp_y;
 		
 			
 		
 		counter := cnt_move;
 		elsif(counter = 100000) then
 			
-			memo_x := x_player;
-			memo_y := y_player;
-			memo_x_2 := x_player_2;
-			memo_y_2 := y_player_2;
-			pos_diff_x := memo_x - memo_x_2 ;
-			pos_diff_y := memo_y - memo_y_2 ;
+			tmp_x := x_player;
+			tmp_y := y_player;
+			tmp_x_2 := x_player_2;
+			tmp_y_2 := y_player_2;
+			pos_diff_x := tmp_x - tmp_x_2 ;
+			pos_diff_y := tmp_y - tmp_y_2 ;
 			
 			if pos_diff_x > 30 OR pos_diff_x < -29 OR pos_diff_y > 30 OR pos_diff_y < -30 then
 			is_touching_left := '1' ;
@@ -89,26 +89,26 @@ begin
 			end if;
 			
 			if go_left = '1' AND is_touching_left = '1' then
-			memo_x := memo_x - 1;
+			tmp_x := tmp_x - 1;
 			end if; 
 			
 			if go_right = '1' AND is_touching_right = '1' then 
-				memo_x := memo_x + 1 ;
+				tmp_x := tmp_x + 1 ;
 			end if;
 			
 			if go_up = '1' then 
-				memo_y := memo_y + 1;
+				tmp_y := tmp_y + 1;
 			end if;
 			
 			if go_down = '1' then
-				memo_y := memo_y - 1;
+				tmp_y := tmp_y - 1;
 			end if;
 			
 			is_touching_left := '0'; 
 			is_touching_right := '0';
 		
-			x_player <= memo_x;
-			y_player <= memo_y; 
+			x_player <= tmp_x;
+			y_player <= tmp_y; 
 		
 		counter := 0;
 		cnt_move <= counter ; 
@@ -119,7 +119,7 @@ begin
 		end if;
 		
 				
-	end process get_player_position;
+	end process move;
 	
 	
 	
