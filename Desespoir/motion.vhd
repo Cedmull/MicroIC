009cd1ba9a4aa6d00b0a240ef5ae76d277ce6a43
AlexHannotte <64020619+AlexHannotte@users.noreply.github.com>
2020-08-11 22:43:52 +0200

    latest version
    bords, start et init, collisions

--- Desespoir/motion.vhd
+++ Desespoir/motion.vhd
@@ -27,9 +27,9 @@ port
 	start_game	  : in std_logic; -- '0' to start
 	-- OUTPUTs:
 	-- Player position and orientation
-	x_player     : buffer natural range 0     to 600;
+	x_player     : buffer natural range 0     to 800;
 	y_player     : buffer natural range 0     to 600;
-	x_player_2 	 : in natural 		range 0 		to 600;
+	x_player_2 	 : in natural 		range 0 		to 800;
 	y_player_2	 : in natural 		range 0 		to 600;
 	cnt_move  	 : buffer natural range 0 		to 100000
 );
@@ -41,7 +41,7 @@ begin
 	
 	move: process
 		                  -- These variables are used in order to avoid moving the player while being 
-			variable tmp_x : integer range 0 	to 600;		-- displayed and also prevents the player to "enter" in a wall
+			variable tmp_x : integer range 0 	to 800;		-- displayed and also prevents the player to "enter" in a wall
 			variable tmp_y : integer range 0 	to 600;
 			variable tmp_x_2 : integer range 0 to 600;
 			variable tmp_y_2 : integer range 0 to 600;
@@ -50,6 +50,8 @@ begin
 			variable pos_diff_y : integer range -1000  to 1000;
 			variable is_touching_left : std_logic:= '0';
 			variable is_touching_right : std_logic:='0';
+			variable is_touching_up : std_logic := '0';
+			variable is_touching_down : std_logic := '0';
 	
 	begin
 		
@@ -67,7 +69,6 @@ begin
 			
 			
 			y_player <= tmp_y;
-		
 			
 		
 		counter := cnt_move;
@@ -80,32 +81,46 @@ begin
 			pos_diff_x := tmp_x - tmp_x_2 ;
 			pos_diff_y := tmp_y - tmp_y_2 ;
 			
-			if pos_diff_x > 30 OR pos_diff_x < -29 OR pos_diff_y > 30 OR pos_diff_y < -30 then
+			if pos_diff_x > 30 OR pos_diff_x < -29 OR pos_diff_y > 29 OR pos_diff_y < -29 then 
 			is_touching_left := '1' ;
-			
 			end if;
-			if pos_diff_x < -30 OR pos_diff_x > 29 OR pos_diff_y > 30 OR pos_diff_y < -30 then
+			
+			if pos_diff_x < -30 OR pos_diff_x > 29 OR pos_diff_y > 29 OR pos_diff_y < -29 then
 			is_touching_right := '1' ;
 			end if;
 			
-			if go_left = '1' AND is_touching_left = '1' then
+			if pos_diff_y < -30 OR pos_diff_y > 29 OR pos_diff_x > 29 OR pos_diff_x < -29 then
+			is_touching_up := '1';
+			elsif ( is_touching_left ='0' OR is_touching_right ='0' ) then
+			is_touching_up := '1' ;
+			end if;
+			
+			if pos_diff_y > 30 OR pos_diff_y < -29 OR pos_diff_x > 29 OR pos_diff_x < -29 then
+			is_touching_down := '1' ;
+			elsif is_touching_left ='0' OR is_touching_right ='0' then
+			is_touching_down := '1' ;
+			end if;
+			
+			if go_left = '1' AND is_touching_left = '1' AND tmp_x > 85 then
 			tmp_x := tmp_x - 1;
 			end if; 
 			
-			if go_right = '1' AND is_touching_right = '1' then 
+			if go_right = '1' AND is_touching_right = '1' AND tmp_x < 715 then 
 				tmp_x := tmp_x + 1 ;
 			end if;
 			
-			if go_up = '1' then 
+			if go_up = '1' AND is_touching_up = '1' AND tmp_y < 555 then 
 				tmp_y := tmp_y + 1;
 			end if;
 			
-			if go_down = '1' then
+			if go_down = '1' AND is_touching_down = '1' AND tmp_y > 45 then
 				tmp_y := tmp_y - 1;
 			end if;
 			
 			is_touching_left := '0'; 
 			is_touching_right := '0';
+			is_touching_down := '0'; 
+			is_touching_up := '0';
 		
 			x_player <= tmp_x;
 			y_player <= tmp_y; 
