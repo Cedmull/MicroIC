e0a84446948419a43641676de0acac48bb6e52e7
AlexHannotte <64020619+AlexHannotte@users.noreply.github.com>
2020-08-11 18:57:53 +0200

    Update motion.vhd
    Makes move an object. Reset should update both positions

--- Desespoir/motion.vhd
+++ Desespoir/motion.vhd
@@ -5,6 +5,10 @@ use ieee.std_logic_UNSIGNED.ALL;
 
 entity motion is 
 
+generic(
+	constant collision_radius : natural := 30
+	);
+
 port
 (
 	-- INPUTS:
@@ -20,11 +24,13 @@ port
 	
 	-- Control and initialization variables
 	player_flag   : in std_logic; -- '0' for player 1, '1' if player 2
-	start_game	  : buffer std_logic; -- '0' to start
+	start_game	  : in std_logic; -- '0' to start
 	-- OUTPUTs:
 	-- Player position and orientation
-	x_player     : buffer natural range 0     to 800;
-	y_player     : buffer natural range 0     to 600;
+	x_player     : buffer natural range 0     to 600;
+	y_player     : buffer natural range 0     to 500;
+	x_player_2 	 : in natural 		range 0 		to 600;
+	y_player_2	 : in natural 		range 0 		to 500;
 	cnt_move  	 : buffer natural range 0 		to 100000
 );
 end motion;
@@ -35,37 +41,71 @@ begin
 	
 	get_player_position: process
 		                  -- These variables are used in order to avoid moving the player while being 
-			variable memo_x : integer range 0 to 600	;		-- displayed and also prevents the player to "enter" in a wall
-			variable memo_y : integer range 0 to 500;
-			variable counter :integer range 0 to 100000;
+			variable memo_x : integer range 0 	to 600;		-- displayed and also prevents the player to "enter" in a wall
+			variable memo_y : integer range 0 	to 600;
+			variable memo_x_2 : integer range 0 to 600;
+			variable memo_y_2 : integer range 0 to 600;
+			variable counter :integer range 0 	to 100000;
+			variable pos_diff_x : integer range -1000  to 1000;
+			variable pos_diff_y : integer range -1000  to 1000;
+			variable is_touching_left : std_logic:= '0';
+			variable is_touching_right : std_logic:='0';
 	
 	begin
 		
 		wait until rising_edge( CLOCK_50 );
 		
+		if(push_start = '1') then
+			if player_flag = '0' then
+				memo_x := 200;
+				memo_x_2:= 600;
+			else
+				memo_x := 600;
+				memo_x_2 := 200;
+			end if;
+			memo_y := 300;	
+			
+			x_player <= memo_x;
+			y_player <= memo_y;
+		
+			
 		
 		counter := cnt_move;
-		if(counter = 100000) then
+		elsif(counter = 100000) then
 			
 			memo_x := x_player;
 			memo_y := y_player;
+			memo_x_2 := x_player_2;
+			memo_y_2 := y_player_2;
+			pos_diff_x := memo_x - memo_x_2 ;
+			pos_diff_y := memo_y - memo_y_2 ;
 			
-			if go_left = '1' then
-				memo_x := memo_x + 1;
+			if pos_diff_x > 30 OR pos_diff_x < -29 OR pos_diff_y > 30 OR pos_diff_y < -30 then
+			is_touching_left := '1' ;
+			
+			end if;
+			if pos_diff_x < -30 OR pos_diff_x > 29 OR pos_diff_y > 30 OR pos_diff_y < -30 then
+			is_touching_right := '1' ;
+			end if;
+			
+			if go_left = '1' AND is_touching_left = '1' then
+			memo_x := memo_x - 1;
 			end if; 
 			
-			if go_right = '1' then 
-				memo_x := memo_x - 1 ;
+			if go_right = '1' AND is_touching_right = '1' then 
+				memo_x := memo_x + 1 ;
 			end if;
 			
-			if go_up = '1' then
-				memo_y := memo_y - 1;
-			end if;
-			
-			if go_down = '1' then
+			if go_up = '1' then 
 				memo_y := memo_y + 1;
 			end if;
 			
+			if go_down = '1' then
+				memo_y := memo_y - 1;
+			end if;
+			
+			is_touching_left := '0'; 
+			is_touching_right := '0';
 		
 			x_player <= memo_x;
 			y_player <= memo_y; 
