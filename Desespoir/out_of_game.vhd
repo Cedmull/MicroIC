b042b9d19a0ba90fa3c0345f66d576b4d963850e
Cedmull <58887195+Cedmull@users.noreply.github.com>
2020-08-11 22:12:06 +0200

    simplification qui fait marcher

--- Desespoir/out_of_game.vhd
+++ Desespoir/out_of_game.vhd
@@ -15,9 +15,6 @@ end entity out_of_game;
 
 architecture be_cool of out_of_game is 
 	
-	signal is_pushed_1 : std_logic := '0';
-	signal is_pushed_2 : std_logic := '0';
-	signal launch : std_logic;
 	
 begin
 
@@ -27,18 +24,12 @@ begin
 		
 		wait until rising_edge( CLOCK_50 );
 		
-		if(start_game = '1') then
-		
-			is_pushed_1 <= push_start_1 ;
-			is_pushed_2 <= push_start_2 ; 
-		
-			if(is_pushed_1 = '1') OR (is_pushed_2 = '1') then
-				launch <= '0';
-				start_game <= launch;
-			end if;
+		if(push_start_1 = '1') OR (push_start_2 = '1') then
+				
+				start_game <= '0';
+		else
+			start_game <= '1';
 		end if;
 	end process;
-	
-end be_cool;
 	
 end be_cool;
